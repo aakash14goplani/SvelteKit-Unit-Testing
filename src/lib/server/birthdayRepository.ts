f58d4570fa422f7857289319798f46eb7819011b
aakash_goplani_nagarro <aakash.goplani@nagarro.com>
2023-04-28 00:46:09 +0530

    chapter 9 completed

--- src/lib/server/birthdayRepository.ts
+++ src/lib/server/birthdayRepository.ts
@@ -4,14 +4,51 @@ import type { Item } from '$lib/constants/constants';
 const db = new Map();
 
 export const addNew = (item: Item) => {
-	const id = crypto.randomUUID();
-	db.set(id, { ...item, id });
+	const validationResult = validate(item);
+	if (validationResult) {
+		return validationResult;
+	}
+
+	return set(crypto.randomUUID(), item);
 };
 
-export const replace = (id: string, item: Item) => db.set(id, { ...item, id });
+export const replace = (id: string, item: Item) => {
+	if (!has(id)) {
+		return { error: 'An unknown ID was provided.' };
+	}
+
+	const validationResult = validate(item);
+	if (validationResult) {
+		return validationResult;
+	}
+
+	return set(id, item);
+};
 
 export const has = (id: string) => db.has(id);
 
 export const getAll = () => Array.from(db.values());
 
 export const clear = () => db.clear();
+
+const set = (id: string, item: Item) => {
+  const itemWithId = { ...item, id };
+  db.set(id, itemWithId);
+  return itemWithId;
+};
+
+const empty = (value: string) => value === undefined || value === null || value.trim() === '';
+
+const invalidDob = (dob: string) => isNaN(Date.parse(dob));
+
+const validate = ({ name, dob }: { name: string; dob: string }) => {
+	if (empty(name)) {
+		return { error: 'Please provide a name.' };
+	}
+
+	if (invalidDob(dob)) {
+		return {
+			error: 'Please provide a date of birth in the YYYY-MM-DD format.'
+		};
+	}
+};
